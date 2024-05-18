dbd442b382488cfed56b446637dc4b863308b26c
Piotr Maciejewski <pether.maciejewski@gmail.com>
2024-05-18 09:30:07 +0200

    ts fixing 2.0

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,8 @@ import { NextRequest, NextResponse } from "next/server"
 
 const BACKEND_URL = process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL
 const DEFAULT_REGION = process.env.NEXT_PUBLIC_DEFAULT_REGION || "pl"
+const SUPPORTED_LANGUAGES = ["en", "fr", "es", "pl"]
+const DEFAULT_LANGUAGE = "en"
 
 const regionMapCache = {
   regionMap: new Map<string, Region>(),
@@ -17,7 +19,6 @@ async function getRegionMap() {
     !regionMap.keys().next().value ||
     regionMapUpdated < Date.now() - 3600 * 1000
   ) {
-    // Fetch regions from Medusa. We can't use the JS client here because middleware is running on Edge and the client needs a Node environment.
     const { regions } = await fetch(`${BACKEND_URL}/store/regions`, {
       next: {
         revalidate: 3600,
@@ -29,7 +30,6 @@ async function getRegionMap() {
       notFound()
     }
 
-    // Create a map of country codes to regions.
     regions.forEach((region: Region) => {
       region.countries.forEach((c) => {
         regionMapCache.regionMap.set(c.iso_2, region)
@@ -42,11 +42,6 @@ async function getRegionMap() {
   return regionMapCache.regionMap
 }
 
-/**
- * Fetches regions from Medusa and sets the region cookie.
- * @param request
- * @param response
- */
 async function getCountryCode(
   request: NextRequest,
   regionMap: Map<string, Region | number>
@@ -80,9 +75,6 @@ async function getCountryCode(
   }
 }
 
-/**
- * Middleware to handle region selection and onboarding status.
- */
 export async function middleware(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams
   const isOnboarding = searchParams.get("onboarding") === "true"
@@ -95,11 +87,17 @@ export async function middleware(request: NextRequest) {
 
   const countryCode = regionMap && (await getCountryCode(request, regionMap))
 
-  const urlHasCountryCode =
-    countryCode && request.nextUrl.pathname.split("/")[1].includes(countryCode)
+  const urlCountryCode = request.nextUrl.pathname.split("/")[1]?.toLowerCase()
 
-  // check if one of the country codes is in the url
+  // Check if the URL has a supported language code
+  const urlHasSupportedLanguage = SUPPORTED_LANGUAGES.includes(urlCountryCode)
+
+  // Check if the URL has the country code
+  const urlHasCountryCode = urlCountryCode && regionMap.has(urlCountryCode)
+
+  // If the URL has a supported language code and it's valid in the region map, proceed
   if (
+    urlHasSupportedLanguage &&
     urlHasCountryCode &&
     (!isOnboarding || onboardingCookie) &&
     (!cartId || cartIdCookie)
@@ -107,29 +105,24 @@ export async function middleware(request: NextRequest) {
     return NextResponse.next()
   }
 
+  // If no country code is set or if it's unsupported, redirect to the default language
+  const redirectCountryCode = SUPPORTED_LANGUAGES.includes(countryCode)
+    ? countryCode
+    : DEFAULT_LANGUAGE
   const redirectPath =
     request.nextUrl.pathname === "/" ? "" : request.nextUrl.pathname
-
   const queryString = request.nextUrl.search ? request.nextUrl.search : ""
 
-  let redirectUrl = request.nextUrl.href
+  let redirectUrl = `${request.nextUrl.origin}/${redirectCountryCode}${redirectPath}${queryString}`
 
   let response = NextResponse.redirect(redirectUrl, 307)
 
-  // If no country code is set, we redirect to the relevant region.
-  if (!urlHasCountryCode && countryCode) {
-    redirectUrl = `${request.nextUrl.origin}/${countryCode}${redirectPath}${queryString}`
-    response = NextResponse.redirect(`${redirectUrl}`, 307)
-  }
-
-  // If a cart_id is in the params, we set it as a cookie and redirect to the address step.
   if (cartId && !checkoutStep) {
     redirectUrl = `${redirectUrl}&step=address`
     response = NextResponse.redirect(`${redirectUrl}`, 307)
     response.cookies.set("_medusa_cart_id", cartId, { maxAge: 60 * 60 * 24 })
   }
 
-  // Set a cookie to indicate that we're onboarding. This is used to show the onboarding flow.
   if (isOnboarding) {
     response.cookies.set("_medusa_onboarding", "true", { maxAge: 60 * 60 * 24 })
   }
